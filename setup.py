636c0e632c561b6c3e8e955e54e7e29bf1ac805a
Andrea Stagi <stagi.andrea@gmail.com>
2020-02-26 11:20:28 +0100

    Version 0.9.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyphoton',
-    version='0.0.0',
+    version='0.9.0',
     url='',
     install_requires=["requests==2.22.0"],
     description="Photon Python client",
